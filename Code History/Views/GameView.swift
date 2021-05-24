470dd842fc7886be98fba16baf679d4f23fc4077
Matthew Paul O'Brien <matpobrien@gmail.com>
2021-05-24 16:29:52 +0200

    create GameView and GameViewModel, refactor GameView to use QuestionView

--- Code History/Views/GameView.swift
+++ Code History/Views/GameView.swift
@@ -10,42 +10,19 @@ import SwiftUI
 
 struct GameView: View {
     
-    let question = Question(
-        questionText: "What was the first computer bug?",
-        possibleAnswers: ["Ant", "Beetle", "Moth", "Fly"],
-        correctAnswerIndex: 2)
-    
-    @State var mainColor = Color(red: 20/255, green: 28/255, blue: 58/255)
+    @StateObject var viewModel = GameViewModel()
     
     var body: some View {
         ZStack {
-            mainColor.ignoresSafeArea()
-            VStack {
-                Text("1 / 10")
+            GameColor.main.ignoresSafeArea()
+            VStack{
+                Text(viewModel.questionProgressText)
                     .font(.callout)
                     .multilineTextAlignment(/*@START_MENU_TOKEN@*/.leading/*@END_MENU_TOKEN@*/)
                     .padding()
-                Text(question.questionText)
-                    .multilineTextAlignment(/*@START_MENU_TOKEN@*/.leading/*@END_MENU_TOKEN@*/)
-                    .font(.largeTitle)
-                    .padding()
-                Spacer()
-                HStack {
-                    ForEach(0 ..< question.possibleAnswers.count) { answerIndex in
-                        Button(
-                            action: {
-                                print("Tapped on \(question.possibleAnswers[answerIndex])")
-                                mainColor = answerIndex == question.correctAnswerIndex ? GameColor.correctGuess : GameColor.incorrectGuess
-                            },
-                            label: {
-                                ChoiceTextView(choiceText: question.possibleAnswers[answerIndex])
-
-                            }
-                        )
-                    }
-                }
-            }.foregroundColor(.white)
-        }
+                QuestionView(question: viewModel.currentQuestion)
+            }
+        }.foregroundColor(.white)
     }
 }
 
